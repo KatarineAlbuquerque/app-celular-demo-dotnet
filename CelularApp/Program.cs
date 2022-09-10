8bffc02c363744e4887052ef3051c2a752027ab7
KatarineAlbuquerque <katarine.kba@gmail.com>
2022-09-10 15:02:07 -0300

    App Celular .Net

---
+++ CelularApp/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using CelularApp.Model;
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "999997755", modelo: "Modelo 1", imei: "11111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine();
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "888885544", modelo: "Modelo 2", imei: "22222222", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
+
